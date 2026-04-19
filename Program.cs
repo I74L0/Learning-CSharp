5a2dc7287e4fa4f77525804a69c30007af01c081
Italo <i7alog4m3r1@gmail.com>
2026-04-19 15:24:26 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("What is your name?");
+var name = Console.ReadLine();
+var currentDate = DateTime.Now;
+Console.WriteLine($"\nHello, {name}, on {currentDate:d} at {currentDate:t}!");
